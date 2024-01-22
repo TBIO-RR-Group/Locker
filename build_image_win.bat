ec313b866129e8e44b6e539f9de9f77627c62e15
Eric Davis <31807001+EricSDavis@users.noreply.github.com>
2024-01-22 16:11:09 -0500

    Add build and start script for Windows environment.

--- build_image_win.bat
+++ build_image_win.bat
@@ -9,4 +9,19 @@ SET ECR_REGISTRY=483421617021.dkr.ecr.us-east-1.amazonaws.com
 SET LOCAL_TAG=%REGISTRY%/%NAME%:%VERSION%
 SET REGISTRY_TAG=%ECR_REGISTRY%/%NAME%:%VERSION%
 
+REM Build the image
 docker build --build-arg PORT=%PORT% --build-arg SERVER_ADMIN=%SERVER_ADMIN% -t %LOCAL_TAG% -t %REGISTRY_TAG% -t %NAME%:%VERSION% -f Dockerfile .
+
+REM Generate start script through docker container
+docker run --rm %LOCAL_TAG% /locker/gen_start_script win > start_script.bat
+SET START_SCRIPT=start_script.bat
+
+REM Read the raw text into a variable
+set /p RAW_STRING=<%START_SCRIPT%
+
+REM Remove Unix-style byte order mark at the beinning of the file by encoding in UTF8
+chcp 65001
+echo %RAW_STRING% > %START_SCRIPT%
+
+REM Run the start script
+call %START_SCRIPT%
