010e78bf40cf24714d5cc93303f46224c6fc9383
Eric Davis <31807001+EricSDavis@users.noreply.github.com>
2024-01-23 16:23:18 -0500

    Update build and run script, config file, and Dockerfile

--- build_and_run_win.bat
+++ build_and_run_win.bat
@@ -2,9 +2,9 @@
 
 SET PORT=5000
 SET SERVER_ADMIN=andrewsmith_97@yahoo.com
-SET NAME=rr
+SET NAME=locker_rr
 SET VERSION=locker_devtest
-SET REGISTRY=docker.rdcloud.bms.com:443
+SET REGISTRY=dockerreg.example.com:443
 SET ECR_REGISTRY=483421617021.dkr.ecr.us-east-1.amazonaws.com
 SET LOCAL_TAG=%REGISTRY%/%NAME%:%VERSION%
 SET REGISTRY_TAG=%ECR_REGISTRY%/%NAME%:%VERSION%
